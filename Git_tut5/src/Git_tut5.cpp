eb20c6771a30b3516894146a0a6f670d635c9067
helia <helia.azima@ryerson.ca>
2015-10-26 17:12:14 -0400

    Hi everyone

---
+++ Git_tut5/src/Git_tut5.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Git_tut5.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout<< "Salam"<<endl;
+	return 0;
+}
